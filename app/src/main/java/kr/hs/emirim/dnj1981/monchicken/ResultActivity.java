52b39287c38cb1677a7745ec4f7f85fcd3101565
SeonYeong Won <dnj1981@naver.com>
2016-03-28 20:14:08 +0900

    #1 #2 Create Project add Result Activity

---
+++ app/src/main/java/kr/hs/emirim/dnj1981/monchicken/ResultActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.dnj1981.monchicken;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by appcreator03 on 2016. 3. 28..
+ */
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
