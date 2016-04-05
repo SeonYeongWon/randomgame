0132ae1d34bd832f7ae807e58d06880a2ab43612
SeonYeong Won <dnj1981@naver.com>
2016-04-05 20:32:28 +0900

    #4 handle user input(name)

--- app/src/main/java/kr/hs/emirim/dnj1981/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dnj1981/monchicken/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
-        mName.setText("");
+        mName.setText(null);
     }
 
     /**
@@ -37,8 +37,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {
-        Toast.makeText(this,"배고파요!", Toast.LENGTH_LONG).show(); //duration->기간
-        Intent intent=new Intent(this, ResultActivity.class);
-        startActivity(intent);
+        String name=mName.getText().toString();
+//        if(name==null){
+//            Toast.makeText(this,name+"이름을 입력해 주세요!", Toast.LENGTH_LONG).show(); //duration->기간
+//        }
+//        else{
+//            Toast.makeText(this,name+"씨, 배고파요!", Toast.LENGTH_LONG).show(); //duration->기간
+//            Intent intent=new Intent(this, ResultActivity.class);
+//            startActivity(intent);
+//        }
+
+        try{
+            Toast.makeText(this,name+"씨, 배고파요!", Toast.LENGTH_LONG).show(); //duration->기간
+            Intent intent=new Intent(this, ResultActivity.class);
+            startActivity(intent);
+        }catch (NullPointerException e){
+            Toast.makeText(this, "이름을 입력해 주세요!", Toast.LENGTH_LONG).show(); //duration->기간
+
+        }catch (Exception e){
+            Toast.makeText(this, "뭔지 모르지만 잘 안되네요!", Toast.LENGTH_LONG).show(); //duration->기간
+
+        }
     }
+
 }
