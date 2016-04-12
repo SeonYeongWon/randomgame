c82292d3520c9ff4d4c536da1691c999edcc5590
SeonYeong Won <dnj1981@naver.com>
2016-04-12 20:08:23 +0900

    #10 MainActivity change animation ResultActivity

--- app/src/main/java/kr/hs/emirim/dnj1981/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dnj1981/monchicken/MainActivity.java
@@ -21,11 +21,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mName = (EditText) findViewById(R.id.name);
         mButton = (Button) findViewById(R.id.btn_show_me_the_chicken);
-        mButton.setOnClickListener(this);
+        mButton.setOnClickListener(this);   //버튼이 클릭이 되었는지 감시하는 감시자역할 (this)->이벤트 핸들러 클래스 객체가 들어가는 자리(처리자)
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume() { //activity 재귀
         super.onResume();
         mName.setText(null);
     }
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * @param v The view that was clicked.
      */
     @Override
-    public void onClick(View v) {
+    public void onClick(View v) { //버튼 클릭 시
         String name=mName.getText().toString();
 //        if(name==null){
 //            Toast.makeText(this,name+"이름을 입력해 주세요!", Toast.LENGTH_LONG).show(); //duration->기간
@@ -53,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             intent.putExtra("name",name);
             intent.putExtra("age",10);
             startActivity(intent);
+            overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
         }catch (NullPointerException e){
             Toast.makeText(this, "이름을 입력해 주세요!", Toast.LENGTH_LONG).show(); //duration->기간
 
@@ -60,6 +61,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, "뭔지 모르지만 잘 안되네요!", Toast.LENGTH_LONG).show(); //duration->기간
 
         }
+
     }
 
 }
